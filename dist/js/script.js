9d94d90c0ac1f2192775913ac71616dc78b7f0c4
raika maulana <raikamaulana40@gmail.com>
2023-03-18 09:49:45 +0700

    portfolio-tailwindcss

---
+++ dist/js/script.js
@@ -0,0 +1,56 @@
+// Navbar Fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    } 
+    else {
+        header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+}
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click' , function() {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+// Klik di luar hamburger
+window.addEventListener('click' , function(e) {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const hmtl = document.querySelector('html');
+
+    darkToggle.addEventListener('click' , function() {
+        if (darkToggle.checked) {
+            hmtl.classList.add('dark');
+            localStorage.theme = 'dark';
+        } 
+        else {
+            hmtl.classList.remove('dark');
+            localStorage.theme = 'light';
+        }
+    });
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#dc2626',
+        wpu: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+      fontFamily: {
+        handlee: ['Handlee'],
+      }
+    },
+  },
+  plugins: [],
+}
